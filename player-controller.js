ed37699a948bb8b2698f642068335b74a3d31c2c
tomer barnea <tomerbarnea123@gmail.com>
2022-06-13 14:45:07 +0300

    improved jump handler

--- player-controller.js
+++ player-controller.js
@@ -5,38 +5,11 @@ function handleJumpInput(player) {
         player.chargeBar.tick.width = 3.7
         jumpGauge = jumpMaxGauge;
         if (keyPressed[65] && lastKey === 'a') {
-            player.currentSprite = player.sprites.idle.left
-            playAudioOnce('jumpSfx')
-            player.velocity.x = -4 - (jumpGauge / 550)
-            player.velocity.y = -3 - (jumpGauge / 100)
-            if (currentScene === scene6) {
-                player.velocity.y /= 1.6
-                player.velocity.x /= 1.3
-            }
-            player.isOnPlatform = false
-            lastJump = Date.now();
-            keyReleased[87] = false
+            jump(player,'left')
         } else if (keyPressed[68] && lastKey === 'd') {
-            player.currentSprite = player.sprites.idle.right
-            playAudioOnce('jumpSfx')
-            player.velocity.x = 4 + (jumpGauge / 550)
-            player.velocity.y = -3 - (jumpGauge / 100)
-            if (currentScene === scene6) {
-                player.velocity.y /= 1.6
-                player.velocity.x /= 1.3
-            }
-            player.isOnPlatform = false
-            lastJump = Date.now();
-            keyReleased[87] = false
+            jump(player,'right')
         } else {
-            playAudioOnce('jumpSfx')
-            player.velocity.y = -3 - (jumpGauge / 100)
-            if (currentScene === scene6) {
-                player.velocity.y /= 1.6
-            }
-            player.isOnPlatform = false
-            lastJump = Date.now();
-            keyReleased[87] = false
+            jump(player,'middle')
         }
         jumpGauge = 0
         /*If jump Gauge is not at max , Check if player released W key and jump if true  */
@@ -44,41 +17,11 @@ function handleJumpInput(player) {
         if (player.isGrounded || player.isOnPlatform) {
             player.chargeBar.tick.width = 3.7
             if (keyPressed[65] && lastKey === 'a') {
-                player.currentSprite = player.sprites.idle.left
-                playAudioOnce('jumpSfx')
-                player.velocity.x = -4 - (jumpGauge / 550)
-                player.velocity.y = -3 - (jumpGauge / 100)
-                if (currentScene === scene6) {
-                    player.velocity.y /= 1.6
-                    player.velocity.x /= 1.3
-                }
-                player.isJumping = true;
-                player.isOnPlatform = false
-                lastJump = Date.now();
-                keyReleased[87] = false
+                jump(player,'left')
             } else if (keyPressed[68] && lastKey === 'd') {
-                player.currentSprite = player.sprites.idle.right
-                playAudioOnce('jumpSfx')
-                player.velocity.x = 4 + (jumpGauge / 550)
-                player.velocity.y = -3 - (jumpGauge / 100)
-                if (currentScene === scene6) {
-                    player.velocity.y /= 1.6
-                    player.velocity.x /= 1.3
-                }
-                player.isJumping = true;
-                player.isOnPlatform = false
-                lastJump = Date.now();
-                keyReleased[87] = false
+                jump(player,'right')
             } else {
-                playAudioOnce('jumpSfx')
-                player.velocity.y = -3 - (jumpGauge / 100)
-                if (currentScene === scene6) {
-                    player.velocity.y /= 1.6
-                }
-                player.isJumping = true;
-                player.isOnPlatform = false
-                lastJump = Date.now();
-                keyReleased[87] = false
+                jump(player,'middle')
             }
 
         }
@@ -86,3 +29,17 @@ function handleJumpInput(player) {
     }
 }
 
+function jump(player, direction){
+    playAudioOnce('jumpSfx')
+    if(direction === 'left'){
+        player.currentSprite = player.sprites.idle.left
+        player.velocity.x = -4 - (jumpGauge / 550)
+    }else if(direction === 'right'){
+        player.currentSprite = player.sprites.idle.right
+        player.velocity.x = 4 - (jumpGauge / 550)
+    }
+    player.velocity.y = -3 - (jumpGauge / 100)
+    player.isJumping = true;
+    player.isOnPlatform = false
+    lastJump = Date.now();
+}
